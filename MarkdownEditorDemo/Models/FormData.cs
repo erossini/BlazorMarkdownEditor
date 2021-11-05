b0724eba8d99a199a593a9d41006a9346de24e4f
Enrico Rossini <enrico.rossini.uk@live.com>
2021-11-05 16:26:35 +0000

    First upload

---
+++ MarkdownEditorDemo/Models/FormData.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MarkdownEditorDemo.Models
+{
+    public class FormData
+    {
+        public string MarkdownText { get; set; }
+    }
+}
